2cb191b753d9af8b0333136997b360db77c25355
AddChew <addison_chew@outlook.com>
2022-10-25 00:11:57 +0800

    use unittest in functional_tests.py

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,23 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'The install worked successfully! Congratulations!' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Navigate to to-do app homepage
+        self.browser.get('http://localhost:8000')
+
+        # Check page title
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+        
+
+if __name__ == '__main__':
+    unittest.main()
